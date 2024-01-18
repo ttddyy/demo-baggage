92523e3a4428bdc8659ac3870f96102d1ca5f42a
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2024-01-18 12:29:06 -0800

    Init

---
+++ src/main/java/com/example/demobaggage/DemoBaggageApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobaggage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBaggageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBaggageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobaggage/DemoBaggageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobaggage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBaggageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
